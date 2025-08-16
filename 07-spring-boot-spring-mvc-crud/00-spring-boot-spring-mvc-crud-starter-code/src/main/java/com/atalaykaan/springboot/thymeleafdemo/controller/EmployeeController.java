cc046cd66857fae1d9ec7d153116650ff8c996c6
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-08-17 01:38:35 +0300

    spring mvc crud initial exercise

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/atalaykaan/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.atalaykaan.springboot.thymeleafdemo.controller;
+
+import com.atalaykaan.springboot.thymeleafdemo.entity.Employee;
+import com.atalaykaan.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employeeList = employeeService.findAll();
+
+        model.addAttribute("employees" ,employeeList);
+
+        return "list-employees";
+    }
+}
