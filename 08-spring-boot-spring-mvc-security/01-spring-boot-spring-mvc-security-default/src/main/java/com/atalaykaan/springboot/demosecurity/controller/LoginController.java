64a7b6031532c5a627343de6559d4a516f24da5d
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-08-18 02:13:54 +0300

    spring mvc security db exercise

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/atalaykaan/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.atalaykaan.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoggingPage() {
+
+        return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+
+        return "access-denied";
+    }
+}
